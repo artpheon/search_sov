7244dfa314156b17e3dba99eecf9fe51f917b335
Artur <idcdtokms@gmail.com>
2022-01-03 15:32:51 +0300

    added try-except-finally in the main process

--- process.py
+++ process.py
@@ -18,22 +18,27 @@ def make_search_string(query):
 def process():
     search = Search()
     s = get_session()
-    while True:
-        # looking for not-searched queries
-        queries = s.query(SearchRequest).filter(SearchRequest.is_searched == False)
-        for query in queries:
-            search_string = make_search_string(query)
-            print(f"looking for {search_string}")
-            result = search.basic(search_string)
-            query.is_searched = True
-            if result is None:
-                print("not found")
-            else:
-                # binding the request id to the result
-                d = {"request_id": query.id} | result
-                s.add(SearchResponse(**d))
-            s.commit()
-        sleep(5)
+    try:
+        while True:
+            # looking for not-searched queries
+            queries = s.query(SearchRequest).filter(SearchRequest.is_searched == False)
+            for query in queries:
+                search_string = make_search_string(query)
+                print(f"looking for {search_string}")
+                result = search.basic(search_string)
+                query.is_searched = True
+                if result is None:
+                    print("not found")
+                else:
+                    # binding the request id to the result
+                    d = {"request_id": query.id} | result
+                    s.add(SearchResponse(**d))
+                s.commit()
+            sleep(5)
+    except Exception as e:
+        print(e)
+    finally:
+        s.close()
 
 
 if __name__ == "__main__":
